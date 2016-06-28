ea1aa2981442a4b5ae0e929d3f3c914deb8f0f9d
Greg Linklater <greglink49@gmail.com>
2016-06-28 17:42:33 +0200

    feat(ReviveHandler): initial ReviveHandler class

---
+++ handlers/ReviveHandler.js
@@ -0,0 +1,33 @@
+'use strict'
+/**
+ * Dependencies
+ */
+const cwd = process.cwd()
+const path = require('path')
+const firebase = require('firebase')
+const BaseHandler = require(path.join(cwd, 'handlers', 'BaseHandler'))
+const User = require(path.join(cwd, 'models', 'User'))
+
+/**
+ * Scope
+ */
+const scope = 'ReviveHandler'
+
+/**
+ * ReviveHandler
+ */
+class ReviveHandler extends BaseHandler {
+
+  static handle (event) {
+    let handler = new ReviveHandler(event)
+
+    Promise
+      .resolve({handler})
+  }
+
+}
+
+/**
+ * Exports
+ */
+module.exports = ReviveHandler
