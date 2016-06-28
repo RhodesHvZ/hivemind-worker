5b35178817ffab524249fad4c8380be80a35603e
Greg Linklater <greglink49@gmail.com>
2016-06-28 17:01:19 +0200

    feat(Game): Game model

---
+++ models/Game.js
@@ -0,0 +1,55 @@
+'use strict'
+/**
+ * Dependencies
+ */
+const cwd = process.cwd()
+const path = require('path')
+const firebase = require('firebase')
+const Model = require(path.join(cwd, 'models', 'Model'))
+
+/**
+ * Scope
+ */
+const scope = 'Game'
+
+/**
+ * Game
+ */
+class Game extends Model {
+
+  static get (game) {
+    if (!game) {
+      return this.error({
+        scope,
+        message: 'game undefined'
+      })
+    }
+
+    let ref = Game.getRef(game)
+    return new Game(ref)
+  }
+
+  static getRef (game) {
+    return firebase
+      .database()
+      .ref()
+      .child('games')
+      .child(game)
+  }
+
+  mapPlayer (player) {
+    return new Promise((resolve, reject) => {
+      let update = {}
+      update[`players/${player.uid}`] = true
+      this.ref.update(update, () => {
+        resolve(this)
+      })
+    })
+  }
+
+}
+
+/**
+ * Exports
+ */
+module.exports = Game
